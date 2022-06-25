5b33f6a91d9f63d9db94695d423b16b642d6248c
Rajansinghs <93567118+Rajansinghs@users.noreply.github.com>
2022-06-25 22:05:16 +0530

    Add files via upload

---
+++ Assignment/script.js
@@ -0,0 +1,8 @@
+
+/*
+ I just think to maintain the need not trying to write more 
+code in java but I have also learn javasrcipt. 
+*/
+var name= window.prompt("What's your name?");
+
+document.write("Hey ! " + name + ", Good to see.");
